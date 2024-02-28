5d6a6e15968665b48626434fd0f23f80ece1c9a9
ss-proshore <shyam.bhattarai@proshore.eu>
2024-02-28 20:30:58 +0545

    setup databas

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
 Rails.application.routes.draw do
+  get 'registration/new'
  
   get "up" => "rails/health#show", as: :rails_health_check
 
@@ -10,4 +11,7 @@ Rails.application.routes.draw do
 
   # get "about-us", to: "about#index", as: :about
   get "about" => "about#index", as: :about
+
+  get "sign_up" => "registration#new"
+  post "sign_up" => "registration#create"
 end
